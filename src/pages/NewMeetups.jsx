ff9366df76e7f1b7057cf985029ad33b6bcd4981
Shkatylo4ka <Shkatylo4ka@gmail.com>
2024-02-28 22:40:34 +0300

    feat: get data

--- src/pages/NewMeetups.jsx
+++ src/pages/NewMeetups.jsx
@@ -1,9 +1,11 @@
+import {useNavigate} from 'react-router-dom';
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
+    const navigate = useNavigate();
     const addMeetupHandler = (meetupData) => {
         fetch(
-            'https://maxreact-7bbcd-default-rtdb.firebaseio.com/meetups',
+            'https://maxreact-7bbcd-default-rtdb.firebaseio.com/meetups.json',
             {
                 method: 'POST',
                 body: JSON.stringify(meetupData),
@@ -11,7 +13,9 @@ const NewMeetupPage = () => {
                     'Content-Type': 'application/json'
                 }
             }
-        );
+        ).then(() =>{
+            navigate('/');
+        });
     }
     return (
         <section>
